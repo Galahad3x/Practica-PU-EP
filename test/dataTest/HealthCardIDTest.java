e083c489300ff1f906f7601a007e4865a51b5fd5
Ruyercito <56358111+Ruyercito@users.noreply.github.com>
2021-01-02 19:03:07 +0100

    Test HealthCardID & ProductID

--- test/dataTest/HealthCardIDTest.java
+++ test/dataTest/HealthCardIDTest.java
@@ -3,13 +3,11 @@ import data.HealthCardID;
 import exceptions.NullArgumentException;
 import exceptions.WrongFormatException;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.function.Executable;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 public class HealthCardIDTest {
     @Test
-    void HealthCardTest() throws WrongFormatException {
+    void NullArgumentTest() throws Exception {
         String personalIDTest = null;
         try {
             HealthCardID hct = new HealthCardID(personalIDTest);
@@ -20,6 +18,23 @@ public class HealthCardIDTest {
     }
 
     @Test
-    void name() {
+    void WrongFormatTest() throws Exception{
+        String personalIDTest = "12345678923473";
+        try {
+            HealthCardID hct = new HealthCardID(personalIDTest);
+            fail();
+        } catch (WrongFormatException e) {
+
+        }
+    }
+
+    @Test
+    void CorrectFormatTest() throws Exception{
+        String personalIDTest = "12345678923473";
+        try {
+            HealthCardID hct = new HealthCardID(personalIDTest);
+        } catch (WrongFormatException e) {
+            fail();
+        }
     }
 }
