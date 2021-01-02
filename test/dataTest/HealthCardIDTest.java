c586c407c3dbf47528380b7918eb241f4dd614e6
Armand <56553013+armandciutat@users.noreply.github.com>
2021-01-02 18:56:53 +0100

    HealthCardTest

--- test/dataTest/HealthCardIDTest.java
+++ test/dataTest/HealthCardIDTest.java
@@ -1,9 +1,25 @@
 package dataTest;
+import data.HealthCardID;
+import exceptions.NullArgumentException;
+import exceptions.WrongFormatException;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class HealthCardIDTest {
     @Test
-    void HealthCardTest(){
+    void HealthCardTest() throws WrongFormatException {
+        String personalIDTest = null;
+        try {
+            HealthCardID hct = new HealthCardID(personalIDTest);
+            fail();
+        } catch (NullArgumentException e) {
 
+        }
+    }
+
+    @Test
+    void name() {
     }
 }
