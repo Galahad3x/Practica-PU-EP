1ea3440d21cdbf9c86f9ffa502ea284c5bcd9016
Ruyercito <56358111+Ruyercito@users.noreply.github.com>
2021-01-02 18:16:09 +0100

    Test HealthCardID

--- test/dataTest/HealthCardIDTest.java
+++ test/dataTest/HealthCardIDTest.java
@@ -1,4 +1,16 @@
 package dataTest;
+import data.HealthCardID;
+import exceptions.NullArgumentException;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class HealthCardIDTest {
+    @Test
+    void HealthCardTest(){
+        String personalIDTest;
+
+        HealthCardID hct = new HealthCardID(personalIDTest);
+        assertThrows(hct.getPersonalID(), NullArgumentException);
+    }
 }
