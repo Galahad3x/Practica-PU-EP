3da284bf20a0dbb8c053cda7e5421dc08580b669
Ruyercito <56358111+Ruyercito@users.noreply.github.com>
2021-01-12 17:50:32 +0100

    Medical Prescription Line update

--- src/medicalconsultation/MedicalPrescriptionLine.java
+++ src/medicalconsultation/MedicalPrescriptionLine.java
@@ -1,4 +1,19 @@
 package medicalconsultation;
 
 public class MedicalPrescriptionLine {
+    private dayMoment dayMoment;
+    private float duration;
+    private String instructions;
+    private float dose;
+    private float freq;
+    private FqUnit fqUnit;
+
+    public MedicalPrescriptionLine(dayMoment dayMoment, float duration, String instructions, float dose, float freq, FqUnit fqUnit){
+        this.dayMoment = dayMoment;
+        this.duration = duration;
+        this.instructions = instructions;
+        this.dose = dose;
+        this.freq = freq;
+        this.fqUnit = fqUnit;
+    }
 }
