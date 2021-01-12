42a62fc096885a425294fe136c114fdbbfdba60b
Galahad3x <joelaumedes@gmail.com>
2021-01-12 11:46:53 +0100

    afegides excepcions i continuat ConsultationTerminal

--- src/ConsultationTerminal.java
+++ src/ConsultationTerminal.java
@@ -1,14 +1,50 @@
+import data.DigitalSignature;
+import data.HealthCardID;
 import exceptions.*;
+import medicalconsultation.MedicalPrescription;
+import services.HealthNationalService;
+import services.ScheduledVisitAgenda;
 
 import java.net.ConnectException;
+import java.util.Date;
 
 public class ConsultationTerminal {
 
-    public void initRevision() throws NullArgumentException, HealthCardException, ConnectException {
+    public ScheduledVisitAgenda agenda;
+    public HealthNationalService hns;
+
+    public DigitalSignature eSignature;
+
+    private MedicalPrescription mp;
+
+    public void initRevision() throws NullArgumentException, HealthCardException, ConnectException, NotValidePrescriptionException {
+        //Obtenir el CIP del pacient per mitjà de l'AVC per descarregar la eRecepta
+        HealthCardID patientID = agenda.getHealthCardID();
+
+        //Descarregar la eRecepta per mitjà dl HNS
+        setMedicalPrescription(hns.getePrescription(patientID));
+
+        //Mostrar eRecepta en pantalla
+        mp.print();
+    }
+
+    public void initPrescriptionEdition() throws AnyCurrentPrescriptionException, NotFinishedTreatmentException {
 
     }
 
-    public void enterMedicineGuidelines() throws IncorrectTakingGuidelinesException, NullArgumentException {
+    public void searchForProducts(String keyword) throws AnyMedicineSearchException, ConnectException {
+
+    }
+
+    public void selectProduct(int option) throws AnyMedicineSearchException, ConnectException {
+
+    }
+
+    public void enterMedicineGuidelines(String[] instruc) throws IncorrectTakingGuidelinesException, NullArgumentException, AnySelectedMedicineException {
+
+    }
+
+    public void enterTreatmentEndingDate(Date date) throws IncorrectEndingDateException {
 
     }
 
@@ -18,5 +54,13 @@ public class ConsultationTerminal {
 
     public void getePrescription(){
 
+    }
+
+    public void printePresc() throws PrintingException {
+        //Nothing
+    }
+
+    public void setMedicalPrescription(MedicalPrescription mp){
+        this.mp = mp;
     }
 }
