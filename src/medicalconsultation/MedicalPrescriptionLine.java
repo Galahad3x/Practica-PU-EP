fc56c0d1d03bc468b79603effb0ed5d0586178a2
Galahad3x <joelaumedes@gmail.com>
2021-01-14 20:12:55 +0100

    Arreglat error de MedicalPrescriptionLine

--- src/medicalconsultation/MedicalPrescriptionLine.java
+++ src/medicalconsultation/MedicalPrescriptionLine.java
@@ -1,12 +1,15 @@
 package medicalconsultation;
 
+import data.ProductID;
 import exceptions.NullArgumentException;
 
 public class MedicalPrescriptionLine {
     private TakingGuideline guideline;
+    private ProductID prID;
 
-    public MedicalPrescriptionLine(dayMoment dayMoment, float duration, String instructions, float dose, float freq, FqUnit fqUnit) throws NullArgumentException {
+    public MedicalPrescriptionLine(dayMoment dayMoment, float duration, String instructions, float dose, float freq, FqUnit fqUnit, ProductID prID) throws NullArgumentException {
         this.guideline = new TakingGuideline(dayMoment, duration, instructions, dose, freq, fqUnit);
+        this.prID = prID;
     }
 
     public void setGuideline(TakingGuideline guideline) {
@@ -15,5 +18,13 @@ public class MedicalPrescriptionLine {
 
     public TakingGuideline getGuideline() {
         return guideline;
+    }
+
+    public void setPrID(ProductID prID) {
+        this.prID = prID;
+    }
+
+    public ProductID getPrID() {
+        return prID;
     }
 }
