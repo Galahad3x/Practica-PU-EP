b273fa10a8f77aa70967eb3ab19135b143f601b0
Armand <56553013+armandciutat@users.noreply.github.com>
2021-01-14 18:36:36 +0100

    Creat posologyTest(de prova, sense implementació)

---
+++ test/medicalconsultationTest/PosologyTest.java
@@ -0,0 +1,33 @@
+package medicalconsultationTest;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PosologyTest {
+
+    //Gett
+    @Test
+    void getDoseTest() {
+    }
+
+    @Test
+    void setDoseTest() {
+    }
+
+    @Test
+    void getFreqTest() {
+    }
+
+    @Test
+    void setFreqTest() {
+    }
+
+    @Test
+    void getFreqUnitTest() {
+    }
+
+    @Test
+    void setFreqUnitTest() {
+    }
+}
